aa64235b5af8e93a5f2de0042b83beab925f67c8
alexwander <ryndyuk.alexander@gmail.com>
2016-04-29 23:48:10 +0300

    bot message props

--- bot.js
+++ bot.js
@@ -54,3 +54,11 @@ var globalCurrencyList =  {
     'BIC': 0.0
 };
 
+
+bot.getMe().then(function(me)
+{
+    console.log('Hello! My name is %s!', me.first_name);
+    console.log('My id is %s.', me.id);
+    console.log('And my username is @%s.', me.username);
+    globalBotUserName = me.username;
+});
