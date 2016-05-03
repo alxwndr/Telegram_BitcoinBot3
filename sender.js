52e44dcf80e2b39448b588bf0bbb8f414622b8f7
alexwander <ryndyuk.alexander@gmail.com>
2016-05-03 13:51:41 +0300

    cleanup func

--- sender.js
+++ sender.js
@@ -1,3 +1,11 @@
 /**
  * Created by alexandr on 03.05.2016.
  */
+function trimEachString(aString)
+{
+    return aString.split('\n').map(function(aLine)
+    {
+        aLine = aLine.trim();
+        return aLine;
+    }).join('\n');
+}
