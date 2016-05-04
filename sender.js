4d2af6660879b0da2bbc3d3cc477b5e9af4a861a
alexwander <ryndyuk.alexander@gmail.com>
2016-05-04 13:53:28 +0300

    add check results func

--- sender.js
+++ sender.js
@@ -4,3 +4,8 @@
 let sender = require(__dirname + '/sender');
 let MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://localhost:27017/bcratebot';
+
+
+add new Boolean() function function_name (argument) {
+    // body...
+}
