5ebc3d72c02d51938ae4616f5db9ad04b25a1efb
alexwander <ryndyuk.alexander@gmail.com>
2016-05-07 15:19:05 +0300

    set up server

--- setWebhook.js
+++ setWebhook.js
@@ -1,3 +1,16 @@
 /**
  * Created by alexandr on 07.05.2016.
  */
+'use strict';
+
+let https = require('https');
+let token = require(__dirname + '/config.json').token;
+let options = {
+    host: 'api.telegram.org',
+    port: 443,
+    // Убрать хук
+    // path: '/bot' + token + '/setWebhook?url=',
+    path: '/bot' + token + '/setWebhook?url=https://isprogfun.ru/' + token,
+    method: 'GET'
+};
+
