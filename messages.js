42c9be78058ef7ef1796041783bca894eb029310
alexwander <ryndyuk.alexander@gmail.com>
2016-05-11 15:31:08 +0300

    add webhooks func

--- messages.js
+++ messages.js
@@ -6,3 +6,13 @@ var token = 'TOKEN';
 
 var Bot = require('node-telegram-bot-api'),
 var bot;
+
+if(process.env.NODE_ENV === 'production') {
+    bot = new Bot(token);
+    bot.setWebHook('https://api.telegram.org/bot' + bot.token);
+}
+else {
+    bot = new Bot(token, { polling: true });
+}
+// bot.setWebHook('https://api.telegram.org/bot' + bot.token);
+console.log('bot server started...');
